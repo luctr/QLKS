0c745fb86c73ff5f3792456e6175722b95dfbc05
luctr <dinh.luc.3192@gmail.com>
2021-07-25 14:56:48 +0700

    edit file

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package repository;
+
+
+import java.util.Optional;
+
+public interface UserRepository <T> {
+
+    Iterable<T> findAll();
+
+    Optional<T> findById(Long id);
+
+    void save(T t);
+
+    void delete(Long id);
+}
