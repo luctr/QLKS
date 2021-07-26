b865eccfc884e9d59319756d190f34fd029403df
thantai01 <thantaihp2011@gmail.com>
2021-07-26 14:56:54 +0700

    hung commit IGeneralService

---
+++ src/main/java/com/codegym/service/order/IOrderService.java
@@ -0,0 +1,8 @@
+package com.codegym.service.order;
+
+import com.codegym.model.Orders;
+import com.codegym.service.IGeneralService;
+
+public interface IOrderService extends IGeneralService<Orders> {
+
+}
